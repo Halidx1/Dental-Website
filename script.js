d7b182e6355272edbe16e913f3dba0dce1372878
Halidx1 <halitereglglu01@gmail.com>
2026-07-11 19:49:16 +0300

    announcements and news were added along with sliding background hero

--- script.js
+++ script.js
@@ -68,3 +68,15 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+// Hero Slider Otomatik Geçiş
+let currentSlide = 0;
+const slides = document.querySelectorAll('.hero-slide');
+
+function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+// 5 Saniyede bir dönsün
+setInterval(nextSlide, 5000);
